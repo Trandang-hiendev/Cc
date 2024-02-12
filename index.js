1d031150d963342c0e93044c583b36617003df57
Danghiendevbot001 <135580941+Trandang-hiendev@users.noreply.github.com>
2024-02-12 16:53:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,120 +1,42 @@
-const Discord = require('discord.js-selfbot-v13');
-const fs = require('fs');
-const client = new Discord.Client({
-  readyStatus: false,
-  checkUpdate: false
+require('dotenv').config();
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Self Bot Rich Presence working');
 });
-const keepAlive = require('./server.js');
-keepAlive();
+app.listen(process.env.PORT, () => {
+    console.log('Self Bot Rich Presence working on port ' + process.env.PORT);
+});
 
-let r;
-let startTimestamp = Date.now();
-let buttonName = 'FACEBOOK';
-let buttonUrl = 'https://www.facebook.com/hine.Wns/';
-let rpcURL = 'https://www.facebook.com/hine.Wns/';
-let rpcType = 'PLAYING'; // COMPETING, WATCHING, PLAYING, STREAMING
-let rpcName = '想你';
-let rpcDetails = 'yeu em trong mo';
-let assetsSmallText = '[SSH: KayT]';
-let assetsLargeText = '.';
-let assetsLargeImage = 'https://media.discordapp.net/attachments/1202612834685689927/1203720324349169735/anime-sad-gif-3.gif?ex=65d21ee9&is=65bfa9e9&hm=fb2e582d07254a4925edd56f84c8433e3923488ddad9a952552df460f9a39485&';
-let assetsSmallImage = 'https://cdn.discordapp.com/emojis/1162207615921696858.gif?quality=lossless&size=48';
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+    checkUpdate: false
+});
 
 client.on('ready', async () => {
-  console.clear();
-  console.log(`${client.user.tag} - rich presence started!`);
-  
-  const timestamps = fs.readFileSync('ghosty.txt', 'utf8').split('\n');
-  const randomTimestamp = timestamps[Math.floor(Math.random() * timestamps.length)];
-  const [hours, minutes, seconds] = randomTimestamp.split(':');
-  startTimestamp = Date.now() - (hours * 3600000 + minutes * 60000 + seconds * 1000);
-  
-  updateRichPresence();
-  
-  client.user.setPresence({ status: "idle" });
+
+    const rpc = new Discord.RichPresence()
+        .setApplicationId('1206534288091250758')
+        .setType('PLAYING')
+        .setURL(`https://www.youtube.com/watch?v=HTp5PH8ot6Q&list=RDGMEMHDXYb1_DDSgDsobPsOFxpA&ab_channel=NightLovell`)
+        .setDetails(`hello i'm Hine`)
+        .setName(`here is my discord`)
+        .setState(`have a nice day`)
+        .setParty({
+            max: 5001,
+            current: 4690,
+            id: Discord.getUUID(),
+        })
+        .setAssetsLargeImage(`mp:attachments/754377615774842923/1083781876352819200/ce050454b14baa8a677c9fe3940bd094.jpg`)
+        .setAssetsLargeText(`dead`)
+        .setAssetsSmallImage(`mp:attachments/754377615774842923/1083782130930303126/e1a431f195998ae2c25ad1fc30f497c3.jpg`)
+        .setAssetsSmallText(`Gay`)
+        .addButton(`Discord Shop`, `https://discord.gg/heprxstore `);
+        .addButton(`Discord Server`, `https://discord.gg/lilbae`);
+    client.user.setActivity(rpc.toJSON());
+
+    console.log(`${client.user.tag} is ready!`);
 });
 
-client.on('message', message => {
-  if (message.author.id === client.user.id) {
-    const args = message.content.split(' ');
-    const command = args[0];
-    
-    if (command === '>simg' && args.length === 2) {
-      const imgUrl = args[1];
-      r.setAssetsSmallImage(imgUrl);
-      client.user.setActivity(r);
-      message.delete();
-    } else if (command === '>time' && args.length === 2) {
-      const newTime = args[1];
-      
-      if (isValidTimestamp(newTime)) {
-        const [hours, minutes, seconds] = newTime.split(':');
-        startTimestamp = Date.now() - (hours * 3600000 + minutes * 60000 + seconds * 1000);
-        
-        updateRichPresence();
-        message.delete();
-      } else {
-        message.edit('Invalid timestamp format. Use HH:MM:SS.');
-      }
-    } else if (command === '>bname' && args.length === 2) {
-      buttonName = args[1];
-      updateRichPresence();
-      message.delete();
-    } else if (command === '>burl' && args.length === 2) {
-      buttonUrl = args[1];
-      updateRichPresence();
-      message.delete();
-    } else if (command === '>rpcurl' && args.length === 2) {
-      rpcURL = args[1];
-      updateRichPresence();
-      message.delete();
-    } else if (command === '>rpctype' && args.length === 2) {
-      rpcType = args[1];
-      updateRichPresence();
-      message.delete();
-    } else if (command === '>rpcname' && args.length === 2) {
-      rpcName = args[1];
-      updateRichPresence();
-      message.delete();
-    } else if (command === '>rpcd' && args.length === 2) {
-      rpcDetails = args.slice(1).join(' ');
-      updateRichPresence();
-      message.delete();
-    } else if (command === '>stext' && args.length === 2) {
-      assetsSmallText = args[1];
-      updateRichPresence();
-      message.delete();
-    } else if (command === '>ltext' && args.length === 2) {
-      assetsLargeText = args[1];
-      updateRichPresence();
-      message.delete();
-    } else if (command === '>limg' && args.length === 2) {
-      assetsLargeImage = args[1];
-      updateRichPresence();
-      message.delete();
-    }
-  }
-});
-function isValidTimestamp(timestamp) {
-  const regex = /^\d{2}:\d{2}:\d{2}$/;
-  return regex.test(timestamp);
-}
-function updateRichPresence() {
-  r = new Discord.RichPresence()
-    .setApplicationId('1203284963856752660')
-    .setType(rpcType)
-    .setURL(rpcURL)
-    .setName(rpcName)
-    .setDetails(rpcDetails)
-    .setStartTimestamp(startTimestamp)
-    .setAssetsLargeImage(assetsLargeImage) 
-    .setAssetsLargeText(assetsLargeText) 
-    .setAssetsSmallImage(assetsSmallImage)
-    .setAssetsSmallText(assetsSmallText)
-    .setButtons([
-      { name: buttonName, url: buttonUrl }
-    ]);
-  client.user.setActivity(r);
-}
-
+//Account Token
 client.login(process.env.TOKEN);
